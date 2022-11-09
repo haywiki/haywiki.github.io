015c1a1b0e5fe76fb2e85aa9bdfdec95bc40295c
Anton Vlasov <druidvav@gmail.com>
2022-11-09 10:53:08 +0400

    mtime logging

--- _plugins/save-mtime.rb
+++ _plugins/save-mtime.rb
@@ -9,7 +9,7 @@ end
 Jekyll::Hooks.register :pages, :post_write do |page|
   if File.file?(page.path)
     dest = page.destination("")
-#     Jekyll.logger.info dest
+    Jekyll.logger.info '       Modify time: ' + dest + ' > ' + page.data['last_modified_at_v2'].to_s
     FileUtils.touch dest, :mtime => page.data['last_modified_at_v2']
   end
 end
